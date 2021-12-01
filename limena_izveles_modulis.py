964cd9999e43b5df488f94fb19a621cdbd381dac
ovxon <67020288+ovxon@users.noreply.github.com>
2021-12-01 13:55:24 +0200

    Pievienoju moduļa kodu

---
+++ limena_izveles_modulis.py
@@ -0,0 +1,15 @@
+def limena_izvele(): #pieprasa lietotājam grūtības līmeni
+    global grutibas_limenis
+    while True:
+        grutibas_limenis = input("Izvēlieties grūtības līmeni no 1-3 (1 ir visvieglākais, 3 - visgrūtākais): ")
+        time.sleep(0.5)
+        try:
+            grutibas_limenis = int(grutibas_limenis)
+            if grutibas_limenis in range(1, 4):
+                break
+            else: 
+                print("Ievadīta pārāk liela vērtība, mēģiniet vēlreiz")
+                time.sleep(0.5)
+        except ValueError:
+            print("Ievadītā vērtība nav skaitlis, mēģiniet vēlreiz")
+            time.sleep(0.5)
