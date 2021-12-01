182ca665675c2dedbc25d5b29d59230bba232a47
ovxon <67020288+ovxon@users.noreply.github.com>
2021-12-01 14:00:44 +0200

    importēju laiku

---
+++ gala_darbs.py
@@ -0,0 +1,20 @@
+import random
+import time
+
+grutibas_limenis = 0
+laukums = []
+redzamais_laukums = []
+rezultats = []
+laukuma_kopija = []
+merkaPozicija_x = 0
+merkaPozicija_y = 0
+merku_skaits = 0
+minejumu_skaits = 0
+punkti = 0
+vards = 0
+izvele = 0
+mape = "faili/"
+
+import limena_izveles_modulis as lvm
+
+lvm.limena_izvele
