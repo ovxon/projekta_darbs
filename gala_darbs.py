af4cd05183996f4ff4d9f6fc2a8163e4067c9b03
ovxon <dclasher22@gmail.com>
2021-12-03 00:52:32 +0200

    pievienoju modulu darbibas

--- gala_darbs.py
+++ gala_darbs.py
@@ -1,20 +1,24 @@
 import random
 import time
+import moduli as mod
 
-laukums = []
-redzamais_laukums = []
 rezultats = []
-laukuma_kopija = []
-merkaPozicija_x = 0
-merkaPozicija_y = 0
 merku_skaits = 0
 minejumu_skaits = 0
 punkti = 0
 vards = 0
 izvele = 0
 mape = "faili/"
-import limena_izveles_modulis as lvm
 
-lvm.limena_izvele()
 
-print(lvm.x)
+mod.limena_izvele()
+
+mod.laukuma_izveide()
+
+mod.merki()
+
+mod.laukuma_izvade()
+
+mod.minesana()
+
+mod.parbaude()
